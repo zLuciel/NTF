80e8b7be354bbe53cacbc56d89659ef472798a24
zLuciel <82103687+zLuciel@users.noreply.github.com>
2023-07-13 04:19:02 -0500

    cambio de icon

--- src/components/Home/Arte/BtnSave.jsx
+++ src/components/Home/Arte/BtnSave.jsx
@@ -1,7 +1,7 @@
 "use client"
 import React from 'react'
 import { AiFillHeart } from 'react-icons/ai';
-import { BiSolidEdit } from 'react-icons/Bi';
+import { FiEdit } from 'react-icons/fi';
 import { BtnSaveStyled, FlexSpanBtn } from './css/BtnSave';
 import { useDispatch } from 'react-redux';
 import { dataEdit } from '@/redux/users/userSlice';
@@ -18,7 +18,7 @@ const BtnSave = ({edit,data}) => {
   return (
     <FlexSpanBtn>
     <BtnSaveStyled>
-      {edit ? <BiSolidEdit onClick={handleEdit} className='icon-favorite lg'/> :  <AiFillHeart className='icon-favorite'/>}
+      {edit ? <FiEdit onClick={handleEdit} className='icon-favorite lg'/> :  <AiFillHeart className='icon-favorite'/>}
     </BtnSaveStyled>
     </FlexSpanBtn>
   )
